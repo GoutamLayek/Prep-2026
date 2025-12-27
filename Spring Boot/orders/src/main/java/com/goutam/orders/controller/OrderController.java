19f188a611c8ff8ce77ef07cc0ac0be7b711c0d2
Goutam Layek <goutamlayek0653@gmail.com>
2025-12-27 16:55:18 +0530

    made changes such that path variables work .. /orders/{id}

--- Spring Boot/orders/src/main/java/com/goutam/orders/controller/OrderController.java
+++ Spring Boot/orders/src/main/java/com/goutam/orders/controller/OrderController.java
@@ -4,20 +4,40 @@ import com.goutam.orders.model.Order;
 import com.goutam.orders.service.OrderService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/orders")
 public class OrderController {
 
     private final OrderService orderService;
 
+    
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
 
-    @GetMapping("/orders")
+    // GET/orders
+    @GetMapping
     public List<Order> getOrders() {
         return orderService.getOrders();
+    }
+
+    // GET/orders/{id}
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOrderById(@PathVariable int id){
+
+        Order order = orderService.getOrderById(id);
+
+        if(order == null){
+            return ResponseEntity
+                .status(404)
+                .body(Map.of("error", "Order not found"));
+        }
+        return ResponseEntity.ok(order);
     }
 }
--- Spring Boot/orders/src/main/java/com/goutam/orders/service/OrderService.java
+++ Spring Boot/orders/src/main/java/com/goutam/orders/service/OrderService.java
@@ -11,7 +11,22 @@ public class OrderService {
     public List<Order> getOrders() {
         return List.of(
                 new Order(101, 1000, "CREATED"),
-                new Order(102, 2500, "PAID")
+                new Order(102, 2500, "PAID"),
+                new Order(103, 2000, "PAID"),
+                new Order(104, 1500, "CREATED"),
+                new Order(105, 1700, "PAID")
         );
+    }
+
+    
+
+    public Order getOrderById(int id) {
+        for (Order order : getOrders()) {
+            System.out.println("Checking order id = " + order.getId());
+            if (order.getId() == id) {
+                return order;
+            }
+        }
+        return null;
     }
 }
